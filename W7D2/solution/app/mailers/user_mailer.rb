3bfb6b9a9aec9636c60238bc254ec3bcacb127ac
Gino Reyes <grmreyes@gmail.com>
2021-03-04 19:26:12 +0800

    add W7D4

---
+++ W7D2/solution/app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+    default from: 'from@example.com'
+    def welcome_email(user)
+        @user = user
+        @url  = 'http://example.com/login'
+        mail(to: 'grmreyes@gmail.com', subject: 'Welcome to My Awesome Site')
+      end
+end
